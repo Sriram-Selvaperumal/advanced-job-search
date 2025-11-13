2baaba4d798d4f1b8adb66c585a447610d320a93
Sriram-Selvaperumal <sriramselvaperumal@icloud.com>
2025-11-13 20:42:55 +0530

    Navbar Created

---
+++ advanced-job-search/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+function Navbar(){
+   return(
+    <div className="navi">
+        <div className="title">
+            <h1>Unemployed</h1>
+        </div>
+    </div>
+   )
+
+}
+
+export default Navbar;
