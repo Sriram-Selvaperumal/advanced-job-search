070860f34c983cf5d84df09ba42a830846dda736
Sriram-Selvaperumal <sriramselvaperumal@icloud.com>
2025-11-15 10:39:24 +0530

    added text

--- advanced-job-search/src/components/Platforms.jsx
+++ advanced-job-search/src/components/Platforms.jsx
@@ -8,20 +8,25 @@ import internshalaLogo from '../assets/Internshala.png';
 function Platforms(){
     return(
         <div className="platforms">
-            <div className='linkedin'>
+            <div className='Linkedin'>
                 <img src={linkedinLogo} alt="LinkedIn" width={200} />
+                <span>LinkedIn</span>
             </div>
-            <div className='naukri'>
+            <div className='Naukri'>
                 <img src={naukriLogo} alt="Naukri" width={200} />
+                <span>Naukri</span>
             </div>
             <div className='Indeed'>
                 <img src={indeedLogo} alt="Indeed" width={200} />
+                <span>Indeed</span>
             </div>
             <div className='Glassdoor'>
                 <img src={glassdoorLogo} alt="Glassdoor" width={200} />
+                <span>Glassdoor</span>
             </div>
             <div className='Internshala'>
                 <img src={internshalaLogo} alt="Internshala" width={200} />
+                <span>Internshala</span>
             </div>
         </div>
     )
