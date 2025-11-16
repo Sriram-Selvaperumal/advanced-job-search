73cf353fe7bef4037ad14db93d05c63470bbd22e
Sriram-Selvaperumal <sriramselvaperumal@icloud.com>
2025-11-16 09:51:24 +0530

    popup scaffolding

---
+++ advanced-job-search/src/components/Popup.jsx
@@ -0,0 +1,15 @@
+import './Popup.css';
+
+function Popup() {
+  return (
+    <div className="popover">
+      <div className="pop" >
+        <span>
+          This Site is locked. The dev got a job, unlike you… hang tight, it’ll be unlocked soon!
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
