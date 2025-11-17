adcc91b7a4b2cadbc118b0f212fc02c6bc82ef9b
Sriram-Selvaperumal <sriramselvaperumal@icloud.com>
2025-11-17 21:09:46 +0530

    added button and script

--- advanced-job-search/src/components/Popup.jsx
+++ advanced-job-search/src/components/Popup.jsx
@@ -1,15 +1,18 @@
 import './Popup.css';
 
-function Popup() {
+function Popup({ onClose }) {
   return (
-    <div className="popover">
-      <div className="pop" >
+    <div className="popover" onClick={onClose}>
+      <div className="pop" onClick={(e) => e.stopPropagation()}>
         <span>
           This Site is locked. The dev got a job, unlike you… hang tight, it’ll be unlocked soon!
         </span>
+
+        <button onClick={onClose}>Okay!</button>
       </div>
     </div>
   );
 }
 
 export default Popup;
+
