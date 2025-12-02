bc35dfe154d503b66de7f0f9e57aedb78e957a9f
Sriram-Selvaperumal <sriramselvaperumal@icloud.com>
2025-12-02 15:09:12 +0530

    update

--- advanced-job-search/src/App.jsx
+++ advanced-job-search/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
+import Platforms from "./components/Platforms";
 import Popup from "./components/Popup";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [showPopup, setShowPopup] = useState(true);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handlePlatformClick = (platform) => {
+    if (platform !== "LinkedIn") {
+      setShowPopup(true);
+    } else {
+      window.open("https://www.linkedin.com", "_blank");
+    }
+  };
 
   return (
     <>
-      {showPopup && (
-        <Popup onClose={() => setShowPopup(false)} />
-      )}
+      <Navbar/>
+      <Platforms onPlatformClick={handlePlatformClick} />
+
+      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
     </>
   );
 }
