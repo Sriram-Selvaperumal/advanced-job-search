7583589b170b858ffaa610323647bb31f40389bb
Sriram-Selvaperumal <sriramselvaperumal@icloud.com>
2025-12-02 15:08:30 +0530

    Update

--- advanced-job-search/src/components/Popup.jsx
+++ advanced-job-search/src/components/Popup.jsx
@@ -5,7 +5,7 @@ function Popup({ onClose }) {
     <div className="popover" onClick={onClose}>
       <div className="pop" onClick={(e) => e.stopPropagation()}>
         <span>
-          This Site is locked. The dev got a job, unlike you… hang tight, it’ll be unlocked soon!
+          This Site is locked. hang tight, it’ll be unlocked soon!
         </span>
 
         <button onClick={onClose}>Okay!</button>
